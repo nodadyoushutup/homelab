fe5b77ae0713a11ec675a15925b13f97c5775a8f
nodadyoushutup <admin@nodadyoushutup.com>
2026-03-07 21:58:43 -0500

    remove old modules

--- terraform/module/grafana/config/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "datasources" {
-  description = "Optional list of Grafana data sources to manage."
-  type        = list(any)
-  default     = []
-}
--- terraform/module/nginx_proxy_manager/config/provider.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  required_providers {
-    nginxproxymanager = {
-      source  = "Sander0542/nginxproxymanager"
-      version = "1.2.2"
-    }
-  }
-}
