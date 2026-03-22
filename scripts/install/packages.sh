2cf5e08167ec22649e212cd01ac6a64418a8feb1
nodadyoushutup <admin@nodadyoushutup.com>
2026-03-22 10:59:25 -0400

    runner/packer: install and validate xorriso for packer builds

--- scripts/install/packages.sh
+++ scripts/install/packages.sh
@@ -8,7 +8,7 @@ trap 'die "failed at line $LINENO"' ERR
 export DEBIAN_FRONTEND=noninteractive
 APT_OPTS=(-y --no-install-recommends -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold")
 SUDO_CMD=()
-PACKAGES=(qemu-guest-agent cloud-guest-utils)
+PACKAGES=(qemu-guest-agent cloud-guest-utils xorriso)
 
 require_cmd() {
   local cmd="$1"
