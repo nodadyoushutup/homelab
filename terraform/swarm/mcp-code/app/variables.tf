aca07b50c678a443e3875eeabe3cbce012a326e2
nodadyoushutup <admin@nodadyoushutup.com>
2026-05-11 16:30:19 -0400

    chore(swarm/mcp-code): default image ghcr.io/nodadyoushutup/mcp-code:0.0.2

--- terraform/swarm/mcp-code/app/variables.tf
+++ terraform/swarm/mcp-code/app/variables.tf
@@ -13,7 +13,7 @@ variable "registry_auth" {
 variable "image_reference" {
   description = "mcp-code aggregate image to run."
   type        = string
-  default     = "ghcr.io/nodadyoushutup/mcp-code:latest"
+  default     = "ghcr.io/nodadyoushutup/mcp-code:0.0.2"
 }
 
 variable "env" {
